f7bdfc4c6ed2829ab227a364baf2e1f78ef7564d
Kamil <bil@int.pl>
2023-12-27 22:31:49 +0100

    Secret key as environment variables.

--- decisionassistant/settings.py
+++ decisionassistant/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-inva^_$_0zzjmb8sl0=9no+o&6h5u++e@2#_*y1uo%0*xe%&sd'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
