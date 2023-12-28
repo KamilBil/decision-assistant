3d8076e6f1760799938e551b6b07a7e93afee33a
Kamil <bil@int.pl>
2023-12-28 15:36:38 +0100

    Allow all to create account.

--- decisionassistant/core/views.py
+++ decisionassistant/core/views.py
@@ -1,9 +1,11 @@
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import UserSerializer
 
 class UserCreate(APIView):
+    permission_classes = [permissions.AllowAny]
+
     def post(self, request, format=None):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
