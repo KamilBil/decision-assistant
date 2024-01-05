eb895bfe7855293b831bb8e045cda3a6ea6f1724
Kamil <bil@int.pl>
2024-01-05 19:24:28 +0100

    Token in swagger.

--- decisionassistant/settings.py
+++ decisionassistant/settings.py
@@ -58,7 +58,13 @@ REST_FRAMEWORK = {
 }
 
 SWAGGER_SETTINGS = {
-   'USE_SESSION_AUTH': False
+    'SECURITY_DEFINITIONS': {
+        'api_key': {
+            'type': 'apiKey',
+            'in': 'header',
+            'name': 'Authorization'
+        }
+    },
 }
 
 SIMPLE_JWT = {
